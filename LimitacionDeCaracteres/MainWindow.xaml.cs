ccfb6513862f88faefe3825b1428fbc45bc0d5d3
Manuel <ma.gonzalez16@iesdoctorbalmis.com>
2020-10-26 19:32:41 +0100

    Agregar archivos de proyecto.

---
+++ LimitacionDeCaracteres/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LimitacionDeCaracteres
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Contador_label.Content = TextBox1.Text.Length;
+
+            if (TextBox1.Text.Length == 140)
+            {
+                TextBox1.IsReadOnly = true;
+            }
+            
+
+        }
+    }
+}
